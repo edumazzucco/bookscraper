3f5af1d177b7da742fa4da8a53f1a96626781d0c
edumazzucco <edumazzucco@hotmail.com>
2023-06-02 20:39:56 -0300

    feat: saving data to mysql using .env key for pw

--- bookscraper/bookscraper/pipelines.py
+++ bookscraper/bookscraper/pipelines.py
@@ -5,7 +5,12 @@
 
 
 # useful for handling different item types with a single interface
+from dotenv import load_dotenv
+import os
+import mysql.connector
 from itemadapter import ItemAdapter
+
+load_dotenv()
 
 
 class BookscraperPipeline:
@@ -64,3 +69,93 @@ class BookscraperPipeline:
             adapter['stars'] = 5
 
         return item
+
+
+class SaveToMySQLPipeline:
+
+    def __init__(self):
+        self.conn = mysql.connector.connect(
+            host='localhost',
+            user='root',
+            password=os.getenv('MYSQL_PASSWORD'),
+            database='books'
+        )
+
+        self.cur = self.conn.cursor()
+
+        # Migration
+        self.cur.execute("""
+        CREATE TABLE IF NOT EXISTS books(
+            id int NOT NULL auto_increment, 
+            url VARCHAR(255),
+            title text,
+            upc VARCHAR(255),
+            product_type VARCHAR(255),
+            price_excl_tax DECIMAL,
+            price_incl_tax DECIMAL,
+            tax DECIMAL,
+            price DECIMAL,
+            availability INTEGER,
+            num_reviews INTEGER,
+            stars INTEGER,
+            category VARCHAR(255),
+            description text,
+            PRIMARY KEY (id)
+        )
+        """)
+
+    def process_item(self, item, spider):
+
+        # Insert data into db
+        self.cur.execute(""" insert into books (
+            url, 
+            title, 
+            upc, 
+            product_type, 
+            price_excl_tax,
+            price_incl_tax,
+            tax,
+            price,
+            availability,
+            num_reviews,
+            stars,
+            category,
+            description
+            ) values (
+                %s,
+                %s,
+                %s,
+                %s,
+                %s,
+                %s,
+                %s,
+                %s,
+                %s,
+                %s,
+                %s,
+                %s,
+                %s
+                )""", (
+            item["url"],
+            item["title"],
+            item["upc"],
+            item["product_type"],
+            item["price_excl_tax"],
+            item["price_incl_tax"],
+            item["tax"],
+            item["price"],
+            item["availability"],
+            item["num_reviews"],
+            item["stars"],
+            item["category"],
+            str(item["description"][0])
+        ))
+
+        self.conn.commit()
+        return item
+
+    def close_spider(self, spider):
+
+        # Close cursor and the connection to db
+        self.cur.close()
+        self.conn.close()
